359061db87b3acfd74bd7620205bdefb4375f25c
YusufIMohamed <Yusuf.Isse.Mohamed@gmail.com>
2022-10-13 15:19:08 +0100

    Clean up comments in frequencies.py
    Removed comments I used comments for debugging.

--- frequencies.py
+++ frequencies.py
@@ -1,11 +1,8 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
     items = [str(i) for i in items]#convert to string
     for x in range(0,len(items)):
-        #print(items[x])
-        #print("This is the number of" +items[x-1] + "there are: " +str(items.count(items[x-1])))
         frequencies[items[x-1]] = items.count(items[x-1])
     return frequencies
