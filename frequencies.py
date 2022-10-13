a0c30e62afc4ebca877753cddadfb40e4ad3a343
YusufIMohamed <Yusuf.Isse.Mohamed@gmail.com>
2022-10-13 15:17:03 +0100

    Update Frequencies function in Frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items = [str(i) for i in items]#convert to string
+    for x in range(0,len(items)):
+        #print(items[x])
+        #print("This is the number of" +items[x-1] + "there are: " +str(items.count(items[x-1])))
+        frequencies[items[x-1]] = items.count(items[x-1])
     return frequencies
